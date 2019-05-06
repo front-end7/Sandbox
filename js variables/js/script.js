cfb81e2cb3c49ca05b1b86da883847d3e8113ff3
alexhateful <alexhateful@gmail.com>
2019-05-06 19:48:07 +0300

    add some js

---
+++ js variables/js/script.js
@@ -0,0 +1,26 @@
+// scripts goes here
+let b;
+// const c;
+
+/**
+ * @param {number} a
+ * @type {number}
+ */
+var a = 5;
+b = 'hello'; // "hello"
+// c = `const`;
+
+/**
+ * @type {string}
+ */
+var c = 7 * a;
+c = b + a;
+
+var bool = true; // false
+
+// alert( c );
+
+var num = prompt("Введите число");
+
+alert(num);
+
