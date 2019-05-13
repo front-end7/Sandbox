385e2ba9662408a73c5832aab0b60336470d4101
alexhateful <alexhateful@gmail.com>
2019-05-13 20:28:30 +0300

    add functions example

--- arrays/js/script.js
+++ arrays/js/script.js
@@ -34,10 +34,99 @@ function c(_m) {
 // }
 
 // example 4
+// var symbol = '*';
+// var stroke = '';
 
-var symbol = '*';
-var stroke = '';
+// for(var i = 0; i < 5; i++) {
+//     console.log(stroke += i+1 + " ");
+// }
 
-for(var i = 0; i < 5; i++) {
-    c(stroke += i+1 + " ");
-}
+// example 5
+// var cols = 4;
+// var rows = 4;
+
+// var buffer = '';
+// var current = 1;
+
+// for (let i = 0; i < rows; i++) {
+    
+//     for (let j = 0; j < cols; j++) {
+        
+//         buffer += current++ + " ";
+    
+//     }
+    
+//     console.log(buffer);
+//     buffer = '';
+
+// }
+
+// example 6
+// var cols = 15;
+// var rows = 13;
+
+// var inner = "+";
+// var outer = "-";
+
+// var buffer = '';
+// var stroke = "";
+
+// for (let i = 0; i < rows; i++) {
+    
+//     for (let j = 0; j < cols; j++) {
+        
+//         if (0 == i || i == rows - 1 || j == 0 || j == cols - 1) {
+
+//             buffer += outer + " ";
+
+//         }
+//         else {
+
+//             buffer += inner + " ";
+
+//         }
+    
+//     }
+
+//     stroke += buffer + "\n";
+//     buffer = '';
+    
+// }
+
+// console.log(stroke);
+
+// example 7
+// var arr = "hello";
+// var arr2 = [234,23423,42,323,45];
+// var arr3 = {
+//     "name": "vasya",
+//     "lastName": "pypkin",
+//     "age": 15,
+//     "skills": [
+//         "html",
+//         "js",
+//         "css"
+//     ]
+// }
+
+// 1)
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// 2)
+// arr2.forEach(element => {
+//     console.log(element);
+// });
+
+// 3)
+// for (const key in arr3) {
+//     if (arr3.hasOwnProperty(key)) {
+//         console.log(key, arr3[key]);
+//     }
+// }
+
+// 4)
+// for (const iterator of arr2) {
+//     console.log(iterator);
+// }
---
+++ random/js/script.js
@@ -0,0 +1,76 @@
+// scripts goes here
+
+// document.getElementById("btn").addEventListener("click", Random);
+
+// function Random() {
+//     var randomNumber = Math.random() * 6; //возвращяет нам число в диапазоне от 0 до 5
+//     var randomRound = Math.ceil(randomNumber); //округляет число до запятой
+//     console.log( randomRound );
+// }
+
+// function Random(min = 21, max = 88) {
+//     var randomNumber = Math.random() * (max - min) + min;
+//     var randomRound = Math.round(randomNumber);
+//     console.log(randomRound);
+// }
+
+function Random() {
+    var randomNumber = Math.random() * (arguments[1] - arguments[0]) + arguments[0];
+    var randomRound = Math.round(randomNumber);
+    // console.log(randomRound);
+    return randomRound;
+}
+
+// // Random(prompt(), prompt());
+// Random(2, 5);
+// var r = Random(24, 51);
+// alert(r);
+
+// var rand = Random(24, 51);
+
+// var number;
+
+// do {
+//     number = prompt("Введите число");
+//     if (number > rand) {
+//         alert("число должно быть меньше");
+//     } else if (number == rand) {
+//         alert("ПАБЕДА!")
+//     } else {
+//         alert("число должно быть больше");
+//     }
+// } while (rand != number);
+
+// function min(_a, _b) {
+//     if (_a > _b) {
+//         return _a;
+//     }
+//     if (_b > _a) {
+//         return _b;
+//     }
+//     // return _a > _b ? _a : _b;
+// }
+
+// var a = setTimeout(() => {
+//     console.log(min(19, 5));
+// }, 3000);
+
+// clearTimeout(a);
+
+
+// setInterval(() => {
+//     console.log(min(19, 5));
+// }, 1000);
+
+// do {
+    
+// } while (confirm() != true);
+
+function ucFirst(_str) {
+    var firstChar = _str.charAt(0); // берем первый символ
+    var firstCharUpperCase = firstChar.toUpperCase(); // возводим первый символ в верхний регистр
+    var replaceFirstChar = _str.replace(firstChar, firstCharUpperCase); // заменяем первый символ на новый
+    return replaceFirstChar; // отправляем на гитхаб
+}
+
+console.log(ucFirst("Пасилий"));
