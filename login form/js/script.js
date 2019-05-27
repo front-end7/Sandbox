027b5494f46cdf10cea41a57494fbd07db560323
alexhateful <alexhateful@gmail.com>
2019-05-27 19:26:13 +0300

    update login example

--- login form/js/script.js
+++ login form/js/script.js
@@ -23,5 +23,22 @@ var users = [
 submit.addEventListener('click', Check);
 
 function Check() {
-    
+    log.innerText = "";
+    var loginCorrect = false;
+    var passwordCorrect = false;
+    for (let i = 0; i < users.length; i++) {
+        const element = users[i];
+        if (element.login == login.value) {
+            loginCorrect = true;
+            if (element.pass == pass.value) {
+                passwordCorrect = true;
+                break;
+            }
+            else {
+                log.innerText += "password incorrect";
+                continue;
+            }
+        }
+    }
+    log.innerText += loginCorrect && passwordCorrect ? "wellcome" : "";
 }
