aef7d2efab2dfd3ddcbd6c82ed0c2f4f5150d493
alexhateful <alexhateful@gmail.com>
2019-06-02 12:48:48 +0300

    finish simpleSlider

--- simpleSlider/js/script.js
+++ simpleSlider/js/script.js
@@ -1,95 +1,136 @@
 $(document).ready(function () {
 
-    $('#slider').prepend('<div id="track"></div>');
+    function Slider(selector, obj = {
+            dots: false,
+            arrows: false
+            }
+        )
+    {
+        let slider = selector,
+            dots = obj.dots,
+            arrows = obj.arrows,
+            trackClass = '.fe7slider__track',
+            dotsClass = '.fe7slider__dots',
+            currentPosition = 0;
+        
+        if (obj.currentPosition) {
+            currentPosition = obj.currentPosition - 1;
+        }
 
-    // получаем общую ширину слайдов для трека
-    var trackWidth = 0;
-    $('img').each(function () {
-        trackWidth += $(this).width();
-        $('#track').append($(this));
+        console.log(currentPosition);
+
+        if (!slider) {
+            console.error("no selector found");
+            return false;
+        }
+        else {
+            $(slider).each(function () {
+                $(this).addClass('fe7slider');
+                $(this).prepend('<div class="fe7slider__track"></div>');
+
+                // получаем общую ширину слайдов для трека
+                var trackWidth = 0,
+                    imgCount = $(this).children('img').length;
+                // SELF - запоминаем текущий слайдер
+                var self = $(this);
+                $(this).children('img').each(function () {
+                    trackWidth += $(this).width();
+                    $(self).children(trackClass).append($(this));
+                });
+                // // присваиваем ширину треку
+                $(this).children(trackClass).css('width', `${trackWidth}px`);
+                // MAKE DOTS
+                if (dots) {
+                    $(this).prepend(`
+                        <ul class="fe7slider__dots">
+                        </ul>
+                    `);
+
+                    for (let i = 0; i < imgCount; i++) {
+                        if (i == currentPosition) {
+                            $(this).children(dotsClass).append('<li class="fe7slider__dot fe7slider__dot--active"></li>');
+                        }
+                        else {
+                            $(this).children(dotsClass).append('<li class="fe7slider__dot"></li>');
+                        }
+                    }
+                    
+                    $(dotsClass).on('click', 'li', function () {
+                        currentPosition = $(this).index();
+                        Move();
+                    })
+                }
+                if (arrows) {
+                    $(this).prepend(`
+                        <button class="fe7slider__arrow fe7slider__arrow--prev">prev</button>
+                        <button class="fe7slider__arrow fe7slider__arrow--next">next</button>
+                    `);
+                    
+                    // обработчик кнопок
+                    $(this).children('.fe7slider__arrow').on('click', function () {
+                        if ($(this).hasClass('fe7slider__arrow--prev')) {
+                            currentPosition--;
+                            // если новый текущий слайд больше чем количество слайдов идем в начало
+                            if (currentPosition == -1) {
+                                currentPosition = imgCount - 1;
+                            }
+                        }
+                        if ($(this).hasClass('fe7slider__arrow--next')) {
+                            currentPosition++;
+                            // если новый текущий слайд больше чем количество слайдов идем в начало
+                            if (currentPosition == imgCount) {
+                                currentPosition = 0;
+                            }
+                        }
+                        Move();
+                    })
+                }
+                // функция перемещения трека до текущего слайда
+                function Move() {
+                    // смещение трека в px
+                    var offset = 0;
+                    // считаем сумму ширин каждой картинки то текущей
+                    $(self).find('img').each(function (i) {
+                        if (i < currentPosition) {
+                            offset += $(this).width();
+                        }
+                    });
+                    
+                    if (offset == trackWidth) {
+                        offset = 0;
+                    }
+                    // записываем смещение в css
+                    $(self).children(trackClass).css('transform', `translateX(-${offset}px)`);
+
+                    if (dots) {
+                        $(self).find(dotsClass).children().each(function () {
+                            $(this).removeClass('fe7slider__dot--active');
+                            if ($(this).index() == currentPosition) {
+                                $(this).addClass('fe7slider__dot--active');
+                            }
+                        })
+                    }
+                }
+                Move();
+
+            })
+        }
+    }
+
+    Slider('.slider2', {
+        dots: true,
+        // arrows: true
     });
 
-    $('#slider').prepend(`
-        <ul id="dots">
-        </ul>
-    `)
+    Slider('.slider1', {
+        // dots: true,
+        arrows: true,
+        currentPosition: 2
+    });
 
-    $('#slider').prepend(`
-        <button id="prev">prev</button>
-        <button id="next">next</button>
-    `)
+    Slider('.slider3', {
+        dots: true,
+        arrows: true
+    });
 
-    // присваиваем ширину треку
-    $('#track').css('width', `${trackWidth}px`);
-    // по умолчанию показываем первый слайд
-    var currentPosition = 0;
-    // функция перемещения трека до текущего слайда
-    function Move() {
-        console.log(currentPosition);
-        // смещение трека в px
-        var offset = 0;
-        // считаем сумму ширин каждой картинки то текущей
-        $('img').each(function (i) {
-            if (i < currentPosition) {
-                offset += $(this).width();
-            }
-        });
-        
-        if (offset == trackWidth) {
-            offset = 0;
-        }
-        // записываем смещение в css
-        $('#track').css('transform', `translateX(-${offset}px)`);
-    }
-
-    function MakeDots() {
-        let buf = '';
-        for (let i = 0; i < $('img').length; i++) {
-            if (i == currentPosition) {
-                buf += '<li class="active"></li>'
-            }
-            else {
-                buf += '<li></li>'
-            }
-        }
-
-        $('#dots').append(buf);
-    }
-
-    function UpdateDots() {
-        $('#dots li').each(function () {
-            $(this).removeClass('active');
-            if ($(this).index() == currentPosition) {
-                $(this).addClass('active');
-            }
-        })
-    }
-
-    MakeDots();
-    // обработчик следующего слайда
-    $('#next').on('click', function () {
-        currentPosition = currentPosition + 1;
-        // если новый текущий слайд больше чем количество слайдов идем в начало
-        if (currentPosition == $('img').length) {
-            currentPosition = 0;
-        }
-        UpdateDots();
-        Move();
-    })
-    // обработчик следующего слайда
-    $('#prev').on('click', function () {
-        currentPosition = currentPosition - 1;
-        // если новый текущий слайд больше чем количество слайдов идем в начало
-        if (currentPosition == -1) {
-            currentPosition = $('img').length -1;
-        }
-        UpdateDots();
-        Move();
-    })
-
-    $('#dots').on('click', 'li', function () {
-        currentPosition = $(this).index();
-        UpdateDots();
-        Move();
-    })
 })
