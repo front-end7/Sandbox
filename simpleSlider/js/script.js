19875d6659d33c255ecb4aae82965c63d820e6e0
alexhateful <alexhateful@gmail.com>
2019-06-02 09:24:09 +0300

    add prev button and update dots functionality

--- simpleSlider/js/script.js
+++ simpleSlider/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
     var currentPosition = 0;
     // функция перемещения трека до текущего слайда
     function Move() {
+        console.log(currentPosition);
         // смещение трека в px
         var offset = 0;
         // считаем сумму ширин каждой картинки то текущей
@@ -25,6 +26,15 @@ $(document).ready(function () {
         // записываем смещение в css
         $('#track').css('transform', `translateX(-${offset}px)`);
     }
+
+    function UpdateDots() {
+        $('#dots li').each(function () {
+            $(this).removeClass('active');
+            if ($(this).index() == currentPosition) {
+                $(this).addClass('active');
+            }
+        })
+    }
     // обработчик следующего слайда
     $('#next').on('click', function () {
         currentPosition = currentPosition + 1;
@@ -32,7 +42,23 @@ $(document).ready(function () {
         if (currentPosition == $('img').length) {
             currentPosition = 0;
         }
+        UpdateDots();
+        Move();
+    })
+    // обработчик следующего слайда
+    $('#prev').on('click', function () {
+        currentPosition = currentPosition - 1;
+        // если новый текущий слайд больше чем количество слайдов идем в начало
+        if (currentPosition == -1) {
+            currentPosition = $('img').length -1;
+        }
+        UpdateDots();
         Move();
     })
 
+    $('#dots').on('click', 'li', function () {
+        currentPosition = $(this).index();
+        UpdateDots();
+        Move();
+    })
 })
